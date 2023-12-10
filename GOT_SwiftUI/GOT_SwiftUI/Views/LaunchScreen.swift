144a91fa80e840cb5b436d0ac2412b48db31c601
silviaITI <silvia.casanovamartinez@gmail.com>
2023-12-10 18:19:19 +0100

    Refactor LaunchScreen

--- GOT_SwiftUI/GOT_SwiftUI/Views/LaunchScreen.swift
+++ GOT_SwiftUI/GOT_SwiftUI/Views/LaunchScreen.swift
@@ -9,49 +9,42 @@ import SwiftUI
 
 struct LaunchScreen: View {
     
-    @StateObject var launchViewModel = LaunchViewModel()
+    
     var body: some View {
         NavigationView {
             VStack {
                 NavigationLink {
                     CharactersView()
                 } label: {
-                   ZStack {
-                        Image("cersei")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 300, height: 300)
-                            .clipShape(Circle())
-                            .opacity(0.6)
-                            .shadow(radius: 15)
-                            
-                        Text("Personajes")
-                            .bold()
-                            .font(.title)
-                    }
+                    itemLaunch(imageUrl: "cersei", text: "Personajes")
                 }
-
                 NavigationLink {
                     ContinentsView()
                 } label: {
-                    ZStack {
-                        Image("Lannister 1")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 300, height: 300)
-                            .clipShape(Circle())
-                            .opacity(0.6)
-                            .shadow(radius: 15)
-                        
-                        Text("Continentes")
-                            .bold()
-                            .font(.title)
-                    }
+                    itemLaunch(imageUrl: "Lannister 1", text: "Continentes")
                 }
             }
-
+        }
+    }
+    
+}
+struct itemLaunch: View {
+    var imageUrl: String
+    var text: String
+    var body: some View {
+        
+        ZStack {
+            Image(imageUrl)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+                .clipShape(Circle())
+                .opacity(0.6)
+                .shadow(radius: 15)
             
-            .navigationTitle("Bienvenido")
+            Text(text)
+                .bold()
+                .font(.title)
         }
     }
 }
