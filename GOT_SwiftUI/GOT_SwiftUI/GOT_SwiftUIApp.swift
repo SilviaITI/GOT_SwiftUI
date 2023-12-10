e89712be373dc72d67939f7ee300cb3232f89fc2
silviaITI <silvia.casanovamartinez@gmail.com>
2023-12-10 10:10:18 +0100

    First commit

---
+++ GOT_SwiftUI/GOT_SwiftUI/GOT_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  GOT_SwiftUIApp.swift
+//  GOT_SwiftUI
+//
+//  Created by Silvia Casanova Martinez on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GOT_SwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
